998f7e7286f32a50fac5a48375eace326d3f6a67
奥城健太郎 <ken09460308@gmail.com>
2019-02-03 09:39:18 +0900

    api変更

--- MapApp/AppDelegate.swift
+++ MapApp/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSServices.provideAPIKey("AIzaSyDJpYcKsp3vGptqW7F971RwFiij8xqTkkw")
+        GMSServices.provideAPIKey("api")
         
         return true
     }
